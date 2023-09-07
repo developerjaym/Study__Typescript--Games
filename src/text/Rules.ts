a0d5f8367920da7783567522d5b81fa5e7b36366
Jay M <jaymansmann@yahoo.com>
2023-09-07 16:11:34 -0400

    improves board colors, shortens rules

--- src/text/Rules.ts
+++ src/text/Rules.ts
@@ -4,16 +4,16 @@ const rules =
 `${Icon.CELEBRATE}GBP${Icon.CELEBRATE}
 
 SUMMARY
-Try to build sequences of five or more dice of your color.
+Build sequences of five or more dice of your color.
 
 PLAY
 •Green goes first.
 •Purple goes second.
 •Tap on an empty square to place the displayed die.
-•Try to build sequences (seq.) of five or more dice.
-  •A sequence can be horizontal, vertical, or diagonal.
-  •A sequence's value is equal to the sum of the dice.
-  •BUT a sequence with a ${Icon.ONE} is worth just 1 point.
+•Try to build sequences (seq) of five or more dice.
+  •A seq can be horizontal, vertical, or diagonal.
+  •A seq's value is equal to the sum of the dice.
+  •BUT a seq with a ${Icon.ONE} is worth just 1 point.
 •Your score is your best seq + your worst seq.
 •Game ends when the board is full.
 •The winner is the player with the highest score.`
