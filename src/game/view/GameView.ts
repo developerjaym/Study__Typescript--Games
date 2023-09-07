5d31f17c541d92a6d31d0cf705a7aacf244524f2
Jay M <jaymansmann@yahoo.com>
2023-09-07 13:53:41 -0400

    scores display

--- src/game/view/GameView.ts
+++ src/game/view/GameView.ts
@@ -7,6 +7,7 @@ import { BoardUI } from "./BoardUI.js";
 import { ControlsUI } from "./ControlsUI.js";
 import { DisplaySquareUI } from "./DisplaySquareUI.js";
 import { Icon } from "./Icon.js";
+import { PlayerDataDisplay } from "./PlayerDataDisplay.js";
 import { Viewable } from "./Viewable.js";
 
 export class GameView implements Viewable {
@@ -14,6 +15,8 @@ export class GameView implements Viewable {
   controls: ControlsUI;
   container: HTMLElement;
   displaySquare: DisplaySquareUI;
+  playerOneData: PlayerDataDisplay;
+  playerTwoData: PlayerDataDisplay;
   constructor(
     private controller: IController,
     private htmlService: HTMLService = injector.getHtmlService()
@@ -26,12 +29,18 @@ export class GameView implements Viewable {
     this.board = new BoardUI(this.controller);
     this.displaySquare = new DisplaySquareUI(this.controller)
     this.container.append(this.board.component);
-    this.container.append(this.displaySquare.component)
+    this.playerOneData = new PlayerDataDisplay(true)
+    this.playerTwoData = new PlayerDataDisplay(false)
+    const bottomContainer = this.htmlService.create("div", ["game__bottom"])
+    bottomContainer.append(this.playerOneData.component, this.displaySquare.component, this.playerTwoData.component)
+    this.container.append(bottomContainer)
   }
   get component(): HTMLElement {
     return this.container;
   }
   onChange(event: GameEvent): void {
+    this.playerOneData.onChange(event)
+    this.playerTwoData.onChange(event)
     if(event.type === GameEventType.END) {
       this.htmlService.showDialog("Game Over", event.message, Icon.CELEBRATE, () => this.controller.onEvent({type: UserEventType.END_GAME}))
       return
---
+++ src/game/view/PlayerDataDisplay.ts
@@ -0,0 +1,31 @@
+import { HTMLService } from "../../service/HTMLService.js";
+import injector from "../../service/Injector.js";
+import { GameEvent } from "../model/GameEvent.js";
+import { Viewable } from "./Viewable.js";
+
+export class PlayerDataDisplay implements Viewable {
+  container: HTMLElement;
+  scoreDisplay: HTMLElement;
+  constructor(
+    private playerId: boolean,
+    private htmlService: HTMLService = injector.getHtmlService()
+  ) {
+    this.container = this.htmlService.create("section", [
+      "player-data-display",
+      `player-data-display--${this.playerId ? 'first' : 'second'}`
+    ]);
+    this.scoreDisplay = this.htmlService.create(
+      "p",
+      ["score"]
+    );
+    this.container.appendChild(this.scoreDisplay);
+  }
+  get component(): HTMLElement {
+    return this.container;
+  }
+  onChange(event: GameEvent): void {
+    console.log('playerDataDisplay update');
+    const player = event.players.find(p => p.id === this.playerId)!
+    this.scoreDisplay.textContent = `${player.highScore + player.lowScore} = ${player.highScore} + ${player.lowScore}`
+  }
+}
