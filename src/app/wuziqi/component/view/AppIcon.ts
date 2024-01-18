549ee353cd46c7c2b83b135252dff8fcfb4772c2
Jay M <jaymansmann@yahoo.com>
2024-01-18 17:53:07 -0500

    improves and makes consistent board game styling

--- src/app/wuziqi/component/view/AppIcon.ts
+++
@@ -1,4 +0,0 @@
-export enum AppIcon {
-    WHITE = "○",
-    BLACK = "●"
-}
--- src/app/wuziqi/component/view/text/Rules.ts
+++ src/app/wuziqi/component/view/text/Rules.ts
@@ -4,13 +4,14 @@ const rules =
 `${SystemIcon.CELEBRATE}Wuziqi${SystemIcon.CELEBRATE}
 
 SUMMARY
-Build sequences of five or more stones of your color.
+Build sequences of five or more circles of your color.
 
 PLAY
 •Green goes first.
 •Purple goes second.
-•Tap on an empty square to place the displayed stone.
-•Try to build sequences (seq) of five or more stones.
+•Tap on an empty circle to place the displayed circle.
+•Try to build sequences (seq) of five or more circles.
+  •A seq must be circles of your color.
   •A seq can be horizontal, vertical, or diagonal.
 •Game ends when the first seq is created.
 •The winner is the player who first forms a seq.`
